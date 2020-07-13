96794151616c834d0dee32bbd30314d15dc47775
roukell <62136980+roukell@users.noreply.github.com>
2020-07-13 18:19:32 +1000

    fixed smooth scrolling bug

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // create a smooth scroll function
 function smoothScroll(target, duration) {
     let clickedTarget = document.querySelector(target);
-    let targetPosition = clickedTarget.getBoundingClientRect().top;
-    let startPosition = window.pageYOffset;
+    let targetPosition = window.scrollY + clickedTarget.getBoundingClientRect().top;
+    // let startPosition = window.pageYOffset;
+    let startPosition = window.scrollY;
     let distance = targetPosition - startPosition;
     let startTime = null;
 
-    console.log(targetPosition);
+    // console.log(targetPosition);
 
-    console.log(targetPosition, startPosition, distance, startTime);
+    // console.log(targetPosition, startPosition, distance, startTime);
 
     function animation(currentTime) {
         if (startTime === null) {
@@ -38,6 +39,7 @@ requestAnimationFrame(animation);
 const clickedAbout = document.querySelector("#nav-about");
 const clickedProject = document.querySelector("#nav-project");
 const clickedContact = document.querySelector("#nav-contact");
+const clickedContactIcon = document.querySelector(".fa-comment-dots")
 
 // when a button is clicked, will ease into the target section
 clickedAbout.addEventListener("click", function(){
@@ -52,3 +54,6 @@ clickedContact.addEventListener("click", function(){
     smoothScroll("#section3-content", 2000);
 })
 
+clickedContactIcon.addEventListener("click", function(){
+    smoothScroll("#section3-content", 2000);
+})
