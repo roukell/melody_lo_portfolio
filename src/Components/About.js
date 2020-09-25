16408ab82290158ceb0bb9b2d51a6f0b53173f9c
roukell <62136980+roukell@users.noreply.github.com>
2020-09-25 18:17:53 +1000

    update

--- src/Components/About.js
+++ src/Components/About.js
@@ -31,7 +31,7 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   <span>{street}<br />
+						   <span>
 						         {city} {state}, {zip}
                    </span><br />
 						   <span>{phone}</span><br />
