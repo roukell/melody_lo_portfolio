72be99e4436cbec693c437b24b32a2e78a594903
roukell <62136980+roukell@users.noreply.github.com>
2020-09-25 12:51:11 +1000

    update portfolio

--- README.md
+++ README.md
@@ -1,44 +1,29 @@
-# Portfolio
+# React Nice Resume     
 
-## Description 
+### DEMO is [here](https://nordicgiant2.github.io/react-nice-resume-page/index.html)
 
-This is Melody's portfolio. I applied knowledge of HTML, CSS, and Javascript that I learned, and created my first responsive portfolio. 
-The contents are responsive to various sizes of browser and device.
+![img](https://github.com/nordicgiant2/react-nice-resume/blob/master/public/images/img.jpg?raw=true)
 
+# About [`particles-bg`](https://github.com/lindelof/particles-bg) library
+#### This project uses the react particle background component library [https://github.com/lindelof/particles-bg](https://github.com/lindelof/particles-bg). It's very simple. Anyone can be a Musketeer with it.
 
-## Table of Contents
+![img](https://github.com/lindelof/particles-bg/raw/master/image/03.jpg?raw=true)
 
+## Description
+This project forks from [https://github.com/tbakerx/react-resume-template](https://github.com/tbakerx/react-resume-template), I made some modifications and adjustments, thanks to the [author](https://github.com/tbakerx).
 
-* [Installation](#installation)
-* [Usage](#usage)
-* [Content](#content)
-* [Author](#author)
+![img](https://github.com/nordicgiant2/react-nice-resume/blob/master/public/images/img2.jpg?raw=true)
 
+## Run Project
+### 1. Clone the project
 
-## Installation
+### 2. Run the project
+```shell
+npm i
+npm start
+```
 
-There are no installation required. Please simply head to the following link:
-https://roukell.github.io/melody_lo_portfolio/
-
-
-## Usage 
-
-To view portfolio, open the link and it will bring you to the home page. 
-You can view contents by scolling down, or click on the buttons in navbar and footer. 
-
-![img](./img/portfolio_screenshot.png)
-
-## Content
-* About me
-* Project
-* Resume
-* Contact Me (*currently under construction)
-* Git Hub
-* LinkedIn
-* Email Me Directly (by clicking the paper airplane icon)
-
-## Author
-Melody Lo
- 
-
-
+### 3. BUild
+```shell
+npm run build
+```
---
+++ src/Components/Header.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import ParticlesBg  from "particles-bg";
+
+
+class Header extends Component {
+  render() {
+   let config = {
+      num: [8, 7],
+      rps: 0.3,
+      radius: [5, 500],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      alpha: [0.6, 0],
+      scale: [.1, 0.4],
+      position: "all",
+      color: ["random", "#f23aa9"],
+      cross: "dead",
+      // emitter: "follow",
+      random: 15
+    };
+
+    if (Math.random() > 0.85) {
+      config = Object.assign(config, {
+        onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(
+            particle.p.x,
+            particle.p.y,
+            particle.radius * 2,
+            particle.radius * 2
+          );
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+        }
+      });
+    }
+
+    if(this.props.data){
+       var project = this.props.data.project;
+       var github = this.props.data.github;
+      var name = this.props.data.name;
+      var description= this.props.data.description;
+      var city= this.props.data.address.city;
+      var networks= this.props.data.social.map(function(network){
+        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+      })
+    }
+
+    return (
+      <header id="home">
+      <ParticlesBg type="custom" config={config} bg={true} />
+      <nav id="nav-wrap">
+         <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" className="nav">
+            <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+            <li><a className="smoothscroll" href="#about">About</a></li>
+	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
+            <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+            {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
+         </ul>
+      </nav>
+
+      <div className="row banner">
+      
+         <div className="banner-text">
+            <h1 className="responsive-headline">{name}</h1>
+            <h3>{description}.</h3>
+            <hr />
+            <ul className="social">
+               {/* <a href={project} className="button btn project-btn"><i className="fa fa-book"></i>Project</a> */}
+               <a href={github} className="button btn github-btn" target="_blank"><i className="fa fa-github"></i>Github</a>
+            </ul>
+         </div>
+      </div>
+
+      <p className="scrolldown">
+         <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+      </p>
+
+   </header>
+    );
+  }
+}
+
+export default Header;
